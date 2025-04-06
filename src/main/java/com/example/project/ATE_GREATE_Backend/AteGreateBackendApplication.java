d81c1f6fdd4ff9c8bd2c3ab0439cfda733f5484e
jiy0712 <s2325@e-mirim.hs.kr>
2025-04-06 16:52:15 +0900

    프로젝트 연결

---
+++ src/main/java/com/example/project/ATE_GREATE_Backend/AteGreateBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.project.ATE_GREATE_Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AteGreateBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AteGreateBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/project/ATE_GREATE_Backend/AteGreateBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.project.ATE_GREATE_Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AteGreateBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
