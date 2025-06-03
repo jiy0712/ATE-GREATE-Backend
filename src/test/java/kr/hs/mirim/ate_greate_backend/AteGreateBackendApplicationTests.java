d2c3894412526008ebc3c9cb579d5329185a2388
jiy0712 <s2325@e-mirim.hs.kr>
2025-06-03 23:34:28 +0900

    ATE-GREATE

---
+++ src/test/java/kr/hs/mirim/ate_greate_backend/AteGreateBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.hs.mirim.ate_greate_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AteGreateBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
