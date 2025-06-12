8f0730c65f056d3ea7a59a45a44ea3b142bd64bc
jiy0712 <s2325@e-mirim.hs.kr>
2025-06-13 01:52:20 +0900

    ATE_GREATE_BACKEND

---
+++ src/main/java/com/mirim/ate_greate_backend/AteGreateBackendApplication.java
@@ -0,0 +1,13 @@
+package com.mirim.ate_greate_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AteGreateBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AteGreateBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mirim/ate_greate_backend/AteGreateBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mirim.ate_greate_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AteGreateBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
