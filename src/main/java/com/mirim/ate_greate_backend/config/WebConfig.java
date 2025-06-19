5f780f3933e5fe6d9b41cc3fb2b8adbd51372747
jiy0712 <s2325@e-mirim.hs.kr>
2025-06-19 21:22:05 +0900

    JWT 관련 어노테이션 삭제

--- src/main/java/com/mirim/ate_greate_backend/config/WebConfig.java
+++ src/main/java/com/mirim/ate_greate_backend/config/WebConfig.java
@@ -1,12 +1,10 @@
 package com.mirim.ate_greate_backend.config;
 
-import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.nio.file.Paths;
 
-@Configuration
 public class WebConfig implements WebMvcConfigurer {
 
     @Override
